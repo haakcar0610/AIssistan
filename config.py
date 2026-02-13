14869cb27ff363ce8fbe3f95daf2cfbb011223c8
haakcar0610 <118806017+haakcar0610@users.noreply.github.com>
2026-02-13 13:24:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from groq import Groq
 # Tokenlar ve sabitler
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
-JSONBIN_ID = os.environ.get("JSONBIN_ID")
-JSONBIN_SECRET = os.environ.get("JSONBIN_SECRET")
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 PORT = int(os.environ.get("PORT", 8080))
 HOST = os.environ.get("RENDER_EXTERNAL_HOSTNAME", "aissistan-v2.onrender.com")
 
